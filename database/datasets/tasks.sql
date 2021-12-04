8bc8e58e3437ba9e3b68b344650f132d8825c40e
VK <belsynth@gmail.com>
2021-12-04 14:17:19 +0300

    Добавляет результат работы конвертера датасетов

---
+++ database/datasets/tasks.sql
@@ -0,0 +1,26 @@
+INSERT INTO tasks (created_at, category_id, description, deadline_at, title, budget, coordinates, customer_id, executor_id, city_id)
+VALUES
+('2019-03-09', 2, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', '2019-11-15', 'enable impactful technologies', 6587, ST_GeomFromText('POINT(6.9641667 158.2083333)'), 13, 4, 105),
+('2019-07-03', 3, 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '2019-12-07', 'exploit revolutionary portals', 2904, ST_GeomFromText('POINT(5.623505 10.2544044)'), 6, 18, 682),
+('2019-06-27', 2, 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '2019-11-23', 'matrix next-generation e-commerce', 1170, ST_GeomFromText('POINT(63.593219 53.9068532)'), 2, 2, 609),
+('2019-01-01', 1, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.', '2019-11-10', 'benchmark plug-and-play infomediaries', 838, ST_GeomFromText('POINT(20.5800358 -75.2435307)'), 2, 16, 492),
+('2019-09-07', 3, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', '2019-12-15', 'integrate cross-platform e-business', 7484, ST_GeomFromText('POINT(14.9326574 -91.6941845)'), 1, 17, 196),
+('2018-11-01', 7, 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '2019-11-24', 'enable dot-com niches', 5725, ST_GeomFromText('POINT(40.163127 116.638868)'), 15, 18, 1078),
+('2019-09-13', 5, 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', '2019-11-19', 'transform web-enabled relationships', 4414, ST_GeomFromText('POINT(44.3794871 20.2638941)'), 16, 5, 424),
+('2019-04-01', 8, 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', '2019-11-14', 'strategize frictionless solutions', 3454, ST_GeomFromText('POINT(-7.3251485 108.3607464)'), 3, 15, 839),
+('2019-03-28', 4, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', '2019-12-12', 'innovate seamless metrics', 3101, ST_GeomFromText('POINT(43 -87.97)'), 14, 2, 539),
+('2019-05-01', 4, 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', '2019-12-19', 'integrate wireless infomediaries', 6562, ST_GeomFromText('POINT(41.3410168 -8.3169303)'), 17, 10, 66);
